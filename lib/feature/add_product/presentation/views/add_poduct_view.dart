f7ef661b218abdf8865079dbb345812f395728b2
mohamed <mezoattalla123@gmail.com>
2024-12-02 16:43:57 +0200

    ✨ feat(add_product): Implements AddProductView scaffold and body
    ✨ feat(add_product): Creates AddProductViewBody with basic form structure

--- lib/feature/add_product/presentation/views/add_poduct_view.dart
+++ lib/feature/add_product/presentation/views/add_poduct_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub_dashboart/feature/add_product/presentation/views/widgets/add_poduct_view_body.dart';
+
+import '../../../../core/utils/app_text_styles.dart';
 
 class AddProductView extends StatelessWidget {
   const AddProductView({super.key});
@@ -6,6 +9,13 @@ class AddProductView extends StatelessWidget {
   static const routeName = '/add-product';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add Product"
+        ,style: TextStyles.heading5Bold,),
+        centerTitle: true,
+      ),
+      body: const AddProductViewBody(),
+    );
   }
 }
