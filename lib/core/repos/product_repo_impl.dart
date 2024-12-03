ec1ed8c66256cf10d9b4c225c06fcf613553c941
mohamed <mezoattalla123@gmail.com>
2024-12-03 16:20:01 +0200

    ✨ feat(core/repos): Add product repository interface and implementation

---
+++ lib/core/repos/product_repo_impl.dart
@@ -0,0 +1,10 @@
+ import 'package:fruit_hub_dashboart/core/repos/product_repo.dart';
+import 'package:fruit_hub_dashboart/feature/add_product/domain/entities/add_product_input_entity.dart';
+
+class ProductRepoImpl implements ProductRepo {
+  @override
+  Future<void> addProduct(AddProductInputEntity addProductInputEntity) {
+    // TODO: implement addProduct
+    throw UnimplementedError();
+  }
+}
