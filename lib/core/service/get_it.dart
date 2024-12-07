69bc8c0e64454569810aa930d2fa7d646122cbdd
mohamed <mezoattalla123@gmail.com>
2024-12-07 22:36:01 +0200

    ✨ [feat](add_product/cubit): Add dependencies injection for add product cubit ✨ [feat](core/get_it): Register image and product repositories in get_it

--- lib/core/service/get_it.dart
+++ lib/core/service/get_it.dart
@@ -1,9 +1,17 @@
+import 'package:fruit_hub_dashboart/core/repos/image_repo_impl.dart';
+import 'package:fruit_hub_dashboart/core/repos/product_repo_impl.dart';
 import 'package:fruit_hub_dashboart/core/service/fire_storage.dart';
 import 'package:fruit_hub_dashboart/core/service/storage_service.dart';
 import 'package:get_it/get_it.dart';
+
+import '../repos/image_repo.dart';
+import '../repos/product_repo.dart';
 
 final getIt = GetIt.instance;
 
 void setupGetIt() {
   getIt.registerSingleton<StorageService>(FireStorageService());
+  getIt.registerSingleton<ImageRepo>(
+      ImageRepoImpl(storageService: getIt.get<StorageService>()));
+  getIt.registerSingleton<ProductRepo>(ProductRepoImpl());
 }
--- lib/feature/add_product/presentation/manage/add_product_cubit/add_product_cubit.dart
+++ lib/feature/add_product/presentation/manage/add_product_cubit/add_product_cubit.dart
@@ -1,8 +1,16 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
+import '../../../../../core/repos/image_repo.dart';
+import '../../../../../core/repos/product_repo.dart';
+
 part 'add_product_state.dart';
 
 class AddProductCubit extends Cubit<AddProductState> {
-  AddProductCubit() : super(AddProductInitial());
+
+  AddProductCubit(super.initialState, this.imageRepo, this.productRepo);
+  final ImageRepo imageRepo ;
+  final ProductRepo productRepo ;
+
+
 }
