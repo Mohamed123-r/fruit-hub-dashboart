15e1d2177f5e9bd641ba0ced79b6823e00205c25
mohamed <mezoattalla123@gmail.com>
2024-12-08 21:47:29 +0200

    ✨ feat(add_product_cubit): Implements addProduct functionality

--- lib/feature/add_product/presentation/manage/add_product_cubit/add_product_cubit.dart
+++ lib/feature/add_product/presentation/manage/add_product_cubit/add_product_cubit.dart
@@ -3,14 +3,30 @@ import 'package:meta/meta.dart';
 
 import '../../../../../core/repos/image_repo.dart';
 import '../../../../../core/repos/product_repo.dart';
+import '../../../domain/entities/add_product_input_entity.dart';
 
 part 'add_product_state.dart';
 
 class AddProductCubit extends Cubit<AddProductState> {
-
   AddProductCubit(super.initialState, this.imageRepo, this.productRepo);
-  final ImageRepo imageRepo ;
-  final ProductRepo productRepo ;
 
+  final ImageRepo imageRepo;
 
+  final ProductRepo productRepo;
+
+  Future<void> addProduct(AddProductInputEntity addProductInputEntity) async {
+    emit(AddProductLoading());
+    var result = await imageRepo.uploadImage(addProductInputEntity.image!);
+    result.fold(
+      (l) => emit(AddProductError(l.message)),
+      (url) async {
+        addProductInputEntity.urlImage = url;
+        var result = await productRepo.addProduct(addProductInputEntity);
+        result.fold(
+          (l) => emit(AddProductError(l.message)),
+          (r) => emit(AddProductSuccess()),
+        );
+      },
+    );
+  }
 }
