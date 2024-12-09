604873595727775bacc0b95e0b8a3a14d5862d3b
mohamed <mezoattalla123@gmail.com>
2024-12-09 23:08:11 +0200

    ✨ [FEAT](add_product/model): Adds `AddProductInputModel`

---
+++ lib/feature/add_product/data/models/add_prodect_input_model.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import '../../domain/entities/add_product_input_entity.dart';
+
+class AddProductInputModel {
+  final String name;
+  final String description;
+  final num price;
+  final File image;
+  final bool isFeatured;
+  final String code;
+  String? urlImage;
+
+  AddProductInputModel(
+      {required this.name,required this.isFeatured,
+        required this.description,
+        required this.price,
+        required this.image,
+        required this.code,
+        this.urlImage});
+
+  factory AddProductInputModel.fromEntity(AddProductInputEntity entity) => AddProductInputModel(
+      name: entity.name,
+      isFeatured: entity.isFeatured,
+      description: entity.description,
+      price: entity.price,
+      image: entity.image,
+      code: entity.code,
+      urlImage: entity.urlImage);
+
+  toMap() => {
+    "name": name,
+    "description": description,
+    "price": price,
+    "image": urlImage,
+    "isFeatured": isFeatured,
+    "code": code,
+  };
+}
