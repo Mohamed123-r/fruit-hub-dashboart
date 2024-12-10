d5a76a326194e376af407acc7ea9bf531ff0145f
mohamed <mezoattalla123@gmail.com>
2024-12-10 17:39:06 +0200

    ✨ [FEAT](core/service): Adds bloc observer and firebase initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub_dashboart/feature/dashboart/presentation/views/dashboard_view.dart';
 
 import 'core/helper_function/on_generate_route.dart';
+import 'core/service/bloc_opesever.dart';
+import 'core/service/get_it.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  Bloc.observer = CustomBlocObserver();
+  setupGetIt();
   runApp(const MyApp());
 }
 
