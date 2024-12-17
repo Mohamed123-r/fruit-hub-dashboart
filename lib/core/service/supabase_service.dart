f0dea26e730c15211c430c182a4bbff1836f97f7
mohamed <mezoattalla123@gmail.com>
2024-12-17 21:08:35 +0200

    ✨ (core/service): Implements image upload functionality using Supabase storage. ✨ (devtools): Adds devtools_options.yaml configuration file.

--- lib/core/service/supabase_service.dart
+++ lib/core/service/supabase_service.dart
@@ -5,9 +5,9 @@ import 'package:fruit_hub_dashboart/core/service/storage_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../utils/keys.dart';
+import 'package:path/path.dart' as b;
 
 class SupabaseService implements StorageService {
-
   static late Supabase _supabase;
 
   static initializeSupabase() async {
@@ -18,8 +18,12 @@ class SupabaseService implements StorageService {
   }
 
   @override
-  Future<String> uploadImage({required File file, required String pass}) {
-    // TODO: implement uploadImage
-    throw UnimplementedError();
+  Future<String> uploadImage({required File file, required String pass}) async {
+    String fileName = b.basename(file.path);
+    String fileExtension = b.extension(file.path);
+    var result = await _supabase.client.storage
+        .from('fruits_images')
+        .upload("$pass/$fileName.$fileExtension", file);
+    return result;
   }
 }
